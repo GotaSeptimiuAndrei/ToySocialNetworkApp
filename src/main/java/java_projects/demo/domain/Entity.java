ecdcf23f5c018f892ee374e748e2eb827890fda2
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-08-06 18:29:41 +0300

    added domain package

---
+++ src/main/java/java_projects/demo/domain/Entity.java
@@ -0,0 +1,29 @@
+package java_projects.demo.domain;
+
+import java.io.Serializable;
+
+public abstract class Entity<ID>  implements Serializable {
+
+    private ID id;
+
+    public Entity(ID id){
+        this.id = id;
+    }
+
+    /**
+     * Return the id of the entity
+     * @return id of chosen type ID
+     */
+    public ID getId(){
+        return id;
+    }
+
+    /**
+     * Set the id of the entity
+     * @param id of chosen type ID
+     */
+    public void setId(ID id){
+        this.id = id;
+    }
+
+}
